60506280846fe157f24fb13f833916f093e50725
alemanedgar006-jpg <alemanedgar006@gmail.com>
2026-09-22 23:18:29 +0200

    Update horizontal.c

--- P1/Ej1/horizontal.c
+++ P1/Ej1/horizontal.c
@@ -3,8 +3,20 @@
 #include <stdlib.h>
 
 int main(){
-
-
-
-  
+  pid_t pid;
+  for(int i=0;i<3;i++){
+    pid=fork();
+    switch(pid){
+      case -1:
+        perror("Error");
+        exit(1);
+      case 0:
+        printf("Hijo creado, PID: %d, padre: %d\n", getpid(),getppid());
+        exit(0);
+      default:
+        printf("Padre ha creado proceso n: %d, con PID: %d\n",i+1,pid);
+        break;
+    } 
+  }
+  exit(0);
 }
