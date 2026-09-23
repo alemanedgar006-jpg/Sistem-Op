6a73f5d4d9533dacc55d792ad11634a10a3c7730
alemanedgar006-jpg <alemanedgar006@gmail.com>
2026-09-23 09:31:42 +0200

    Update vertical.c

--- P1/Ej1/vertical.c
+++ P1/Ej1/vertical.c
@@ -6,9 +6,13 @@ int main(){
   pid_t pid;
   for(int i=0;i<3;i++){
     pid=fork();
-    switch(pid);
-    
-    
+    switch(pid){
+      case -1:
+        perror("ERROR");
+        exit(-1);
+
+      
+    }
   }
 
   
