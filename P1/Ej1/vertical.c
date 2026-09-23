c4862eb6a1fdd6f10415d1cdc02a0ca4035f7648
alemanedgar006-jpg <alemanedgar006@gmail.com>
2026-09-23 09:36:00 +0200

    Update vertical.c

--- P1/Ej1/vertical.c
+++ P1/Ej1/vertical.c
@@ -11,10 +11,10 @@ int main(){
         perror("ERROR");
         exit(1);
       case 0:
-        printf("Hijo de generacion %d creado con id: %d, id padre: %d",i+1, getpid(),getppid());
+        printf("Hijo de generacion %d creado con id: %d, id padre: %d \n",i+1, getpid(),getppid());
         break;
       default:
-        printf("Padre");
+        printf("Padre 1 creado con id: %d",getpid());
         exit(0);
 
       
